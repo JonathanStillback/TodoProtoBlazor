ddfb658bbc2d03c66f785f214a97bd5f337a3e71
Jonathan Stillbäck <jonathan.stillback@empirindustry.com>
2021-05-16 20:48:21 +0200

    Initial commit of generic todo template project

---
+++ Interfaces/ITaskActions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+
+namespace Interfaces
+{
+    public interface ITodoRepository
+    {
+        Todo Create(Todo todo);
+        Todo Get(int id);
+        List<Todo> GetAll();
+        Todo Update(Todo todo);
+        Todo Delete(Todo todo);
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class Todo
+    {
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public TodoStatus Status {get; set;}
+        public List<string> Comments {get; set;}
+    }
+
+    public enum TodoStatus
+    {
+        New,
+        Started,
+        Waiting,
+        Finished
+    }
+}
